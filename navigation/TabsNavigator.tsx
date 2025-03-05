0cf8aefb61f911d52f745851f084a73ad5d69588
FaustinoHMalena <89898690+FaustinoHMalena@users.noreply.github.com>
2025-03-05 11:34:12 -0500

    Create TabsNavigator.tsx

---
+++ navigation/TabsNavigator.tsx
@@ -0,0 +1,35 @@
+// navigation/TabsNavigator.tsx
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ChallengesScreen, PracticeScreen, ProfileScreen } from '../screens';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabsNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const icons = {
+            Challenges: 'gamepad',
+            Practice: 'school',
+            Profile: 'person',
+          };
+          return (
+            <MaterialIcons
+              name={icons[route.name]}
+              size={size}
+              color={color}
+            />
+          );
+        },
+        tabBarActiveTintColor: '#2B78E4',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Challenges" component={ChallengesScreen} />
+      <Tab.Screen name="Practice" component={PracticeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
